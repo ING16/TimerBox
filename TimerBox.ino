e8a87a87e4d46379cfb2b9be1dfd139ebb0a0e21
polypop <p.frits.hansen@gmail.com>
2017-11-03 12:55:25 +0100

    Adding interupt for counter

--- TimerBox.ino
+++ TimerBox.ino
@@ -15,11 +15,12 @@ int lastButtonState = LOW;            // the previous reading from the input pin
 unsigned long lastDebounceTime = 0;   // the last time the output pin was toggled
 unsigned long debounceDelay = 5;      // the debounce time; increase if the output flickers
 bool bottonState = true;
-
+int secondsGone                = 0;
+ 
 //Switch Variables
 unsigned long int timeOn      = 60;     //The switch is on for this many sec/min
 unsigned long int timeOnCnt;
-unsigned long int timeOnDef   = 1000;   //Defines timeOn 1000=sec 60000=min
+unsigned long int timeOnDef   = 60;      //Defines timeOn 1000=sec 60000=min
 unsigned long int bottonTime  = 0;      //The botton wat pressed at this time
 int ledStatus = LOW;                    //Status for the led
 int switchState = HIGH;
@@ -45,6 +46,23 @@ digitalWrite(switchPin, HIGH);
 switchState = HIGH;
 bottonTime = millis();
 timeOnCnt = timeOn; 
+
+//set timer1 interrupt at 1Hz
+cli();//stop interrupts
+TCCR1A = 0;// set entire TCCR1A register to 0
+TCCR1B = 0;// same for TCCR1B
+TCNT1  = 0;//initialize counter value to 0
+// set compare match register for 1hz increments
+OCR1A = 15624;// = (16*10^6) / (1*1024) - 1 (must be <65536)
+// turn on CTC mode
+TCCR1B |= (1 << WGM12);
+// Set CS10 and CS12 bits for 1024 prescaler
+TCCR1B |= (1 << CS12) | (1 << CS10);  
+// enable timer compare interrupt
+TIMSK1 |= (1 << OCIE1A);
+
+sei();//allow interrupts
+
 }
 
 void loop(){
@@ -90,17 +108,7 @@ void bottonHandling(){
   }
 
 void sevenSegmentHandling(){
-  unsigned long currentMillis = millis();
-//  Serial.print("timeOnDef="); Serial.println(timeOnDef,DEC);
-  if (currentMillis - previousMillis >= timeOnDef and switchState == HIGH) {
-//        // save the last time you blinked the LED
-        previousMillis = currentMillis;
-        timeOnCnt--;
-        if (timeOnCnt <= 0){
-         timeOnCnt = 00;
-         switchState = LOW;
-        }
-     }
+
   displayNo(timeOnCnt);
   
 }
@@ -119,26 +127,32 @@ void displayNo(int no) {
     digit1 = 0;
     digit2 = no;
   }
-//  if (currentMillis - lastSsDelayTime >= ssDelayTime) {
-//    lastSsDelayTime = millis();
 
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, MSBFIRST, B00000010);
-    shiftOut(dataPin, clockPin, MSBFIRST, segmentLight[digit1]);
-    digitalWrite(latchPin, HIGH);
-    delay(5);
-    
-//  digitalWrite(latchPin, LOW);
-//  shiftOut(dataPin, clockPin, MSBFIRST, B00000010);
-////    shiftOut(dataPin, clockPin, MSBFIRST, B10000000); //punktum
-//  digitalWrite(latchPin, HIGH);
-//  delay(5);
-    
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, B00000010);
+  shiftOut(dataPin, clockPin, MSBFIRST, segmentLight[digit1]);
+  digitalWrite(latchPin, HIGH);
+  delay(5);
+
   digitalWrite(latchPin, LOW);
   shiftOut(dataPin, clockPin, MSBFIRST, B00000001);
   shiftOut(dataPin, clockPin, MSBFIRST, segmentLight[digit2]);
   digitalWrite(latchPin, HIGH);
   delay(5);
 }
+
+ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz increment counter
+secondsGone++;
+if (secondsGone >= timeOnDef and switchState == HIGH){
+  timeOnCnt--;
+  if (timeOnCnt <= 0){
+      timeOnCnt = 00;
+      secondsGone = 0;
+      switchState = LOW;
+  }
+}
+
+}
+  
 
 
